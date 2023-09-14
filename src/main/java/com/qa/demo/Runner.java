a21cf043344a73b2858bb8223a2b3ee8ac2f1a27
jharry444 <jharrison1@qa.com>
2023-09-14 10:03:46 +0100

    init commit

---
+++ src/main/java/com/qa/demo/Runner.java
@@ -0,0 +1,15 @@
+// Reverse URL syntax (qa.com -> com.qa)
+package com.qa.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        List<String> strings = new ArrayList<>();
+    }
+}
